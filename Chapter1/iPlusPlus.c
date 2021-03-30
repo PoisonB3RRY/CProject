45bea56b2c607573556153b24d4c60dc71a1de0e
nickj <nickjianjian@outlook.com>
2021-03-31 02:05:50 +0800

    i++ and ++i

---
+++ Chapter1/iPlusPlus.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+				iPlusPlus();
+				plusPlusi();
+}
+
+void iPlusPlus()
+{
+				for(int i=0; i<10; i++)
+				{
+								printf("%d\n",i);
+				}
+}
+
+void plusPlusi(){
+				for(int i=0; i<10; ++i)
+				{
+								printf("%d\n",i);
+				}
+}

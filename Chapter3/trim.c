07d44b8240a8bd49ad3ddf2cd553185c496d31fe
nickj <nickjianjian@outlook.com>
2021-04-29 00:59:22 +0800

    break

---
+++ Chapter3/trim.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int trim(char s[]);
+
+int main()
+{
+    char s[100];
+    int c,i,j;
+
+    i=0;
+    j=0;
+
+    while((c=getchar())!=EOF)
+    {
+        s[i]=c;
+        i++;
+    }
+
+    trim(s);
+    
+    while(s[j] != '\0')
+    {
+        putchar(s[j]);
+        j++;
+    }
+    return 0;
+}
+
+int trim(char s[])
+{
+    int n;
+
+    for(n=strlen(s)-1; n>=0; n--)
+    {
+        if(s[n]!='0' && s[n] != ' ' && s[n] != '\n')
+        {
+            break;
+        }
+    }
+    s[n+1] = '\0';
+    return n;
+}

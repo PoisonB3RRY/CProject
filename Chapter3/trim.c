912a2eabd9ee16d310085bd2e51305b7578bd50e
nickj <nickjianjian@outlook.com>
2021-05-04 18:20:52 +0800

    trim format

--- Chapter3/trim.c
+++ Chapter3/trim.c
@@ -6,24 +6,24 @@ int trim(char s[]);
 int main()
 {
     char s[100];
-    int c,i,j;
+    int c, i, j;
 
-    i=0;
-    j=0;
+    i = 0;
+    j = 0;
 
-    while((c=getchar())!=EOF)
+    while ((c = getchar()) != EOF)
     {
-        s[i]=c;
+        s[i] = c;
         i++;
     }
 
     printf("%lu\n", strlen(s));
 
     trim(s);
-    
-    printf("%lu\n",strlen(s));
 
-  /*  while(s[j] != '\0')
+    printf("%lu\n", strlen(s));
+
+    /*  while(s[j] != '\0')
     {
         putchar(s[j]);
         j++;
@@ -35,13 +35,13 @@ int trim(char s[])
 {
     int n;
 
-    for(n=strlen(s)-1; n>=0; n--)
+    for (n = strlen(s) - 1; n >= 0; n--)
     {
-        if(s[n]!='0' && s[n] != ' ' && s[n] != '\n')
+        if (s[n] != '0' && s[n] != ' ' && s[n] != '\n')
         {
             break;
         }
     }
-    s[n+1] = '\0';
+    s[n + 1] = '\0';
     return n;
 }

e047d746e5f20d1b00a5eae595018ca0bb431d14
nickj <nickjianjian@outlook.com>
2021-05-12 22:33:17 +0800

    calculator

--- Chapter4/cal4getop.c
+++ Chapter4/cal4getop.c
@@ -8,4 +8,29 @@ int ungetch(int c);
 int getop(char s[])
 {
     int i, c;
+
+    while((s[0]=c=getchar()) == ' ' || c == '\t')
+    {
+        ;
+    }
+
+    s[1] = '\0';
+    if(!isdigit(c) && c != '.')
+    {
+        return c;
+    }
+
+    i = 0;
+    if(isdigit(c))
+    {
+        while(isdigit(s[++i]) = c = getch())
+            ;
+    }
+    if (c == '.')
+        while(isdigit(s[++i]) = c = getch())
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
 }

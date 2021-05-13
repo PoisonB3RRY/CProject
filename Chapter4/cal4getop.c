1d284642c68d5ddd8cab0452a92197f31e72f501
nickj <nickjianjian@outlook.com>
2021-05-13 13:57:06 +0800

    getop debug

--- Chapter4/cal4getop.c
+++ Chapter4/cal4getop.c
@@ -9,25 +9,25 @@ int getop(char s[])
 {
     int i, c;
 
-    while((s[0]=c=getchar()) == ' ' || c == '\t')
+    while ((s[0] = c = getchar()) == ' ' || c == '\t')
     {
         ;
     }
 
     s[1] = '\0';
-    if(!isdigit(c) && c != '.')
+    if (!isdigit(c) && c != '.')
     {
         return c;
     }
 
     i = 0;
-    if(isdigit(c))
+    if (isdigit(c))
     {
-        while(isdigit(s[++i]) = c = getch())
+        while (isdigit(s[++i] = c = getch()))
             ;
     }
     if (c == '.')
-        while(isdigit(s[++i]) = c = getch())
+        while (isdigit(s[++i] = c = getch()))
             ;
     s[i] = '\0';
     if (c != EOF)

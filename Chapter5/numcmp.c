ad46a24c20c4953126b2ada6978d2d109bb23772
nickj <nickjianjian@outlook.com>
2021-05-31 19:20:01 +0800

    numcmp.c update

--- Chapter5/numcmp.c
+++ Chapter5/numcmp.c
@@ -13,16 +13,16 @@ int numcmp(char *, char *);
 //sort input lines
 main(int argc, char *argv[])
 {
-    int nlines; //number of input lines read
-    int numeric = 0;  //1 if numeric sort
+    int nlines;      //number of input lines read
+    int numeric = 0; //1 if numeric sort
 
-    if (argc > 1 && strcmp(argv[1], "-n") == 0)
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) //判定若含有-n参数则numeric=1，后续比较方法使用numcmp
     {
         numeric = 1;
     }
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
     {
-        qsort((void**) lineptr, 0, nlines -1, (int (*)(void*, void*))(numeric ? numcmp: strcmp));
+        qsort((void **)lineptr, 0, nlines - 1, (int (*)(void *, void *))(numeric ? numcmp : strcmp)); //numric=1  使用numcmp,否则使用strcmp
         writelines(lineptr, nlines);
         return 0;
     }
@@ -37,36 +37,36 @@ main(int argc, char *argv[])
 void qsort(void *v[], int left, int right, int (*comp)(void *, void *))
 {
     int i, last;
-    void swap(void *v[], int ,int);
-    if(left >= right)
+    void swap(void *v[], int, int);
+    if (left >= right)
     {
-        return ;
+        return;
     }
-    swap(v, left, (left + right)/2);
+    swap(v, left, (left + right) / 2);
     last = left;
-    for (i=left + 1; i <= right; i++)
+    for (i = left + 1; i <= right; i++)
     {
-        if((*comp)(v[i], v[left]) < 0)
+        if ((*comp)(v[i], v[left]) < 0)
         {
             swap(v, ++last, i);
         }
     }
     swap(v, left, last);
-    qsort(v, left, last-1, comp);
-    qsort(v, last+1, right, comp);
+    qsort(v, left, last - 1, comp);
+    qsort(v, last + 1, right, comp);
 }
 
-void numcmp(char *s1, char *s2)
+int numcmp(char *s1, char *s2)
 {
     double v1, v2;
 
     v1 = atof(s1);
     v2 = atof(s2);
-    if(v1 < v2)
+    if (v1 < v2)
     {
         return -1;
     }
-    else if(v1>v2)
+    else if (v1 > v2)
     {
         return 1;
     }
@@ -81,7 +81,7 @@ void swap(void *v[], int i, int j)
 
     void *temp;
 
-    temp =  v[i];
+    temp = v[i];
     v[i] = v[j];
     v[j] = temp;
 }

dffb0048a82060c59163d3e6f4456a4544e9ce29
nickj <nickjianjian@outlook.com>
2021-06-03 14:32:14 +0800

    update makepoints

--- Chapter6/makepoint.c
+++ Chapter6/makepoint.c
@@ -6,13 +6,13 @@ struct point
 {
     int x;
     int y;
-}
+};
 
-struct rect {
+struct rect
+{
     struct point pt1;
     struct point pt2;
-}
-
+};
 
 struct point makepoint(int x, int y)
 {
@@ -21,21 +21,27 @@ struct point makepoint(int x, int y)
     temp.x = x;
     temp.y = y;
     return temp;
-}
+};
 
 struct rect screen;
 struct point middle;
 struct point makepoint(int, int);
 
-screen.pt1 = makepoint(0,0);
+screen.pt1 = makepoint(0, 0);
 screen.pt2 = makepoint(XMAX, YMAX);
 
-middle = makepoint((screen.pt1.x + screen.pt2.x)/2, (screen.pt1.y + screen.pt2.y)/2);
+middle = makepoint((screen.pt1.x + screen.pt2.x) / 2, (screen.pt1.y + screen.pt2.y) / 2);
 
 /* addpoints: add two points */
-struct addpoint(struct point pt1, struct point pt2){
+struct addpoint(struct point pt1, struct point pt2)
+{
     pt1.x += pt2.x;
     pt1.y += pt2.y;
     return pt1;
+}
 
+/* ptinrect: return 1 if p in r, 0 if not */
+int ptinrect(struct point p, struct rect r)
+{
+    return p.x >= r.pt1.x && p.x <= r.pt2.x && p.y >= r.pt1.y && r.pt2.y;
 }

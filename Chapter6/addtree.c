b047c3981096aee8898fe8352d187ae581b794bf
nickj <nickjianjian@outlook.com>
2021-06-06 22:40:03 +0800

    tree

---
+++ Chapter6/addtree.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
+
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char *, int);
+
+int main(){
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while(getword(word, MAXWORD) != EOF){
+        if(isalpha(word[0])){
+            root = addtree(root, word);
+        }
+    }
+    treeprint(root);
+    return 0;
+}
+
+struct tnode *talloc(void);
+char *strdup(char *);
+
+//addtree: add a node with w, at or below p
+struct treenode *addtree(struct tnode *p, char *w){
+    int cond;
+
+    if(p == NULL){
+        p = talloc(); //make a new node
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right=NULL;
+    } else if((cond = strcmp(w, p-word)) == 0){
+        p->count++;  //repeated word
+    } else if(cond < 0){
+        p->left = addtree(p->left , w);
+    } else {
+        p->right = addtree(p-right, w);
+    }
+    return p;
+}
+
+// treeprint: in-order print of tree p
+void treeprint(struct tnode *p){
+    if(p != NULL){
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
---
+++ Chapter6/binarytree.c
@@ -0,0 +1,6 @@
+struct tnode{
+    char *word; //point to the text
+    int count; //number of occurrences
+    struct tnode *left; //left child
+    struct tnode *right; //right child
+}

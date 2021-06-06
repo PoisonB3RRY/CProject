14f31a751455891cb4f7c7b158fe9f14f78255c5
nickj <nickjianjian@outlook.com>
2021-06-07 00:56:23 +0800

    addtree debug

--- Chapter6/addtree.c
+++ Chapter6/addtree.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+#include <stdlib.h>
 #define MAXWORD 100
+
+struct tnode {
+    int count;
+    char *word;
+    struct tnode *left;
+    struct tnode *right;
+};
 
 struct tnode *addtree(struct tnode *, char *);
 void treeprint(struct tnode *);
@@ -23,10 +30,10 @@ int main(){
 }
 
 struct tnode *talloc(void);
-char *strdup(char *);
+//char *strdup(char *);
 
 //addtree: add a node with w, at or below p
-struct treenode *addtree(struct tnode *p, char *w){
+struct tnode *addtree(struct tnode *p, char *w){
     int cond;
 
     if(p == NULL){
@@ -34,12 +41,12 @@ struct treenode *addtree(struct tnode *p, char *w){
         p->word = strdup(w);
         p->count = 1;
         p->left = p->right=NULL;
-    } else if((cond = strcmp(w, p-word)) == 0){
+    } else if((cond = strcmp(w, p->word)) == 0){
         p->count++;  //repeated word
     } else if(cond < 0){
         p->left = addtree(p->left , w);
     } else {
-        p->right = addtree(p-right, w);
+        p->right = addtree(p->right, w);
     }
     return p;
 }
@@ -51,4 +58,42 @@ void treeprint(struct tnode *p){
         printf("%4d %s\n", p->count, p->word);
         treeprint(p->right);
     }
+}
+
+//talloc: make a tnode
+struct tnode *talloc(void){
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim)
+{
+    int c, getch(void);
+    void ungetch(int);
+    char *w = word;
+
+    while (isspace(c = getch()))
+    {
+        ;
+    }
+    if (c != EOF)
+    {
+        *w++ = c;
+    }
+    if (!isalpha(c))
+    {
+        *w = '\0';
+        return c;
+    }
+    for (; --lim > 0; w++)
+    {
+        if (!isalnum(*w = getch()))
+        {
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
 }

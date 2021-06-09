54fd7ac8c4980045d91d397db730e3a730e153e0
nickj <nickjianjian@outlook.com>
2021-06-09 23:54:28 +0800

    Chapter7 start

---
+++ Chapter7/lower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int c;
+
+    while ((c=getchar()) != EOF){ //getchar()输入的字符会放在键盘缓冲区中
+        putchar(tolower(c));
+    }
+
+    return 0;
+}

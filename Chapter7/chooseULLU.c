46e850094ca48169795eb2701b6b3495b225da1c
nickj <nickjianjian@outlook.com>
2021-06-22 10:13:23 +0800

    update chooseULLU.C

--- Chapter7/chooseULLU.c
+++ Chapter7/chooseULLU.c
@@ -2,19 +2,22 @@
 #include <ctype.h>
 #include <string.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int c;
 
     argv = argv + 1; //argv指针跳至第二个
 
-    while((c=getchar())!=EOF){
-        if(strcmp(*argv,"LU") == 0){
+    while ((c = getchar()) != EOF)
+    {
+        if (strcmp(*argv, "LU") == 0)
+        {
             c = toupper(c);
         }
-        if(strcmp(*argv,"UL") == 0){
+        if (strcmp(*argv, "UL") == 0)
+        {
             c = tolower(c);
         }
         putchar(c);
     }
     return 0;
-}

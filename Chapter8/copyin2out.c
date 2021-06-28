223df44dbb860db4b236cebcf8fa3ea07326e724
nickj <nickjianjian@outlook.com>
2021-06-28 23:40:31 +0800

    Chapter 8 start

---
+++ Chapter8/copyin2out.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define BUFSIZ 1000
+
+int main(){
+    char buf[BUFSIZ];
+    int n;
+
+    while((n = read(0, buf, BUFSIZ)) > 0){
+        write(1, buf, n);
+    }
+
+    return 0;
+}
